be5dfb3d5350219e6c043e4299cb2790a2cbc721
Bharadwaj <kunabharadwaj520@gmail.com>
2023-04-30 23:05:41 +0100

    nav bar added

---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+function teamPage() {
+  return (
+    <div>
+      <h1>Our team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+        officiis consequuntur, dolorum libero recusandae numquam quaerat
+        repudiandae sunt! Illum dolore et quaerat. Mollitia, ullam nemo
+        molestias ab vel repudiandae ad.
+      </p>
+    </div>
+  );
+}
+
+export default teamPage;
